38f502c777063202e878f1140f5fae21affc39a5
muchlist <whois.muchlis@gmail.com>
2021-07-21 16:11:12 +0800

    vendor check service

---
+++ lib/src/api/services/vendor_service.dart
@@ -0,0 +1,56 @@
+import '../filter_models/check_filter.dart';
+import '../http_client.dart';
+import '../json_models/request/vendor_req.dart';
+import '../json_models/response/message_resp.dart';
+import '../json_models/response/vendor_check_list_resp.dart';
+import '../json_models/response/vendor_check_resp.dart';
+import '../json_parsers/json_parsers.dart';
+import '../json_parsers/vendor_check_parser.dart';
+
+class VendorCheckService {
+  const VendorCheckService();
+
+  Future<MessageResponse> createVendorCheck(bool isVirtual) {
+    var isVirtualString = "";
+    if (isVirtual) {
+      isVirtualString = "yes";
+    }
+    return RequestREST(
+        endpoint: "/vendor-check?virtual=$isVirtualString",
+        data: {}).executePost<MessageResponse>(MessageParser());
+  }
+
+  Future<VendorCheckDetailResponse> getVendorCheck(String id) {
+    return RequestREST(endpoint: "/vendor-check/$id")
+        .executeGet<VendorCheckDetailResponse>(VendorCheckParser());
+  }
+
+  Future<MessageResponse> deleteVendorCheck(String id) {
+    return RequestREST(endpoint: "/vendor-check/$id")
+        .executeDelete<MessageResponse>(MessageParser());
+  }
+
+  Future<VendorCheckListResponse> findVendorCheck(FilterCheck f) {
+    var query = "";
+    if (f.branch != null) {
+      query = query + "branch=${f.branch}&";
+    }
+    if (f.start != null) {
+      query = query + "start=${f.start}&";
+    }
+    if (f.end != null) {
+      query = query + "end=${f.end}&";
+    }
+    if (f.limit != null) {
+      query = query + "limit=${f.limit}&";
+    }
+    return RequestREST(endpoint: "/vendor-check?$query")
+        .executeGet<VendorCheckListResponse>(VendorCheckListParser());
+  }
+
+  Future<VendorCheckDetailResponse> updateVendorCheck(
+      VendorUpdateRequest payload) {
+    return RequestREST(endpoint: "/vendor-check-update", data: payload.toJson())
+        .executePost<VendorCheckDetailResponse>(VendorCheckParser());
+  }
+}
