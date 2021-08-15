bde867e73cdd3313b334a246915987a2f6c6c766
muchlist <whois.muchlis@gmail.com>
2021-08-15 14:29:30 +0800

    altai and cctv check service update

---
+++ lib/src/api/services/altai_maint_service.dart
@@ -0,0 +1,68 @@
+import 'package:risa2/src/api/json_models/request/altai_maintenance_req.dart';
+import 'package:risa2/src/api/json_models/response/altai_maintenance_resp.dart';
+
+import '../filter_models/check_filter.dart';
+import '../http_client.dart';
+import '../json_models/request/vendor_req.dart';
+import '../json_models/response/cctv_maintenance_resp.dart';
+import '../json_models/response/main_maintenance_list_resp.dart';
+import '../json_models/response/message_resp.dart';
+import '../json_parsers/json_parsers.dart';
+
+class AltaiMaintService {
+  const AltaiMaintService();
+
+  Future<MessageResponse> createAltaiMaintenance(bool isQuartal, String name) {
+    return RequestREST(
+            endpoint: isQuartal ? "/altai-phy-check" : "/phy-check-quarter",
+            data: <String, dynamic>{name: name})
+        .executePost<MessageResponse>(const MessageParser());
+  }
+
+  Future<AltaiMaintDetailResponse> getAltaiMaintenance(String id) {
+    return RequestREST(endpoint: "/altai-phy-check/$id")
+        .executeGet<AltaiMaintDetailResponse>(const AltaiMaintParser());
+  }
+
+  // {{url}}/api/v1/phy-check-finish/60f7bd8d52ffaa4bbb3c3997
+  Future<AltaiMaintDetailResponse> finishAltaiMaintenance(String id) {
+    return RequestREST(endpoint: "/altai-phy-check-finish/$id")
+        .executeGet<AltaiMaintDetailResponse>(const AltaiMaintParser());
+  }
+
+  Future<MessageResponse> deleteAltaiMaintenance(String id) {
+    return RequestREST(endpoint: "/altai-phy-check/$id")
+        .executeDelete<MessageResponse>(const MessageParser());
+  }
+
+  Future<MainMaintenanceListResponse> findAltaiMaintenance(FilterCheck f) {
+    String query = "";
+    if (f.branch != null) {
+      query = query + "branch=${f.branch}&";
+    }
+    if (f.start != null) {
+      query = query + "start=${f.start}&";
+    }
+    if (f.end != null) {
+      query = query + "end=${f.end}&";
+    }
+    if (f.limit != null) {
+      query = query + "limit=${f.limit}&";
+    }
+    return RequestREST(endpoint: "/altai-phy-check?$query")
+        .executeGet<MainMaintenanceListResponse>(const AltaiMaintListParser());
+  }
+
+  Future<AltaiMaintDetailResponse> updateAltaiMaint(
+      AltaiMaintUpdateRequest payload) {
+    return RequestREST(
+            endpoint: "/altai-phy-check-update", data: payload.toJson())
+        .executePost<AltaiMaintDetailResponse>(const AltaiMaintParser());
+  }
+
+  Future<MessageResponse> bulkUpdateVendorCheck(
+      BulkAltaiMaintUpdateRequest payload) {
+    return RequestREST(endpoint: "/bulk-phy-update", data: payload.toJson())
+        .executePost<MessageResponse>(const MessageParser());
+  }
+}
---
+++ lib/src/api/services/cctv_maint_service.dart
@@ -0,0 +1,66 @@
+import 'package:risa2/src/api/json_models/request/cctv_maintenance_req.dart';
+
+import '../filter_models/check_filter.dart';
+import '../http_client.dart';
+import '../json_models/request/vendor_req.dart';
+import '../json_models/response/cctv_maintenance_resp.dart';
+import '../json_models/response/main_maintenance_list_resp.dart';
+import '../json_models/response/message_resp.dart';
+import '../json_parsers/json_parsers.dart';
+
+class CCTVMaintService {
+  const CCTVMaintService();
+
+  Future<MessageResponse> createCctvMaintenance(bool isQuartal, String name) {
+    return RequestREST(
+            endpoint: isQuartal ? "/phy-check" : "/phy-check-quarter",
+            data: <String, dynamic>{name: name})
+        .executePost<MessageResponse>(const MessageParser());
+  }
+
+  Future<CCTVMaintDetailResponse> getCctvMaintenance(String id) {
+    return RequestREST(endpoint: "/phy-check/$id")
+        .executeGet<CCTVMaintDetailResponse>(const CCTVMaintParser());
+  }
+
+  // {{url}}/api/v1/phy-check-finish/60f7bd8d52ffaa4bbb3c3997
+  Future<CCTVMaintDetailResponse> finishCctvMaintenance(String id) {
+    return RequestREST(endpoint: "/phy-check-finish/$id")
+        .executeGet<CCTVMaintDetailResponse>(const CCTVMaintParser());
+  }
+
+  Future<MessageResponse> deleteCctvMaintenance(String id) {
+    return RequestREST(endpoint: "/phy-check/$id")
+        .executeDelete<MessageResponse>(const MessageParser());
+  }
+
+  Future<MainMaintenanceListResponse> findCctvMaintenance(FilterCheck f) {
+    String query = "";
+    if (f.branch != null) {
+      query = query + "branch=${f.branch}&";
+    }
+    if (f.start != null) {
+      query = query + "start=${f.start}&";
+    }
+    if (f.end != null) {
+      query = query + "end=${f.end}&";
+    }
+    if (f.limit != null) {
+      query = query + "limit=${f.limit}&";
+    }
+    return RequestREST(endpoint: "/phy-check?$query")
+        .executeGet<MainMaintenanceListResponse>(const CCTVMaintListParser());
+  }
+
+  Future<CCTVMaintDetailResponse> updateCctvMaint(
+      CCTVMaintUpdateRequest payload) {
+    return RequestREST(endpoint: "/phy-check-update", data: payload.toJson())
+        .executePost<CCTVMaintDetailResponse>(const CCTVMaintParser());
+  }
+
+  Future<MessageResponse> bulkUpdateVendorCheck(
+      BulkCCTVMaintUpdateRequest payload) {
+    return RequestREST(endpoint: "/bulk-phy-update", data: payload.toJson())
+        .executePost<MessageResponse>(const MessageParser());
+  }
+}
